c684703714f22058bb95de4b552d346a88a9840c
Michael Wautier <wautiermichael@gmail.com>
2020-12-02 12:56:23 +0100

    gif-jsx

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -4,11 +4,7 @@ class Gif extends Component {
   render() {
     const src = `https://media4.giphy.com/media/${this.props.id}/giphy.gif`;
     return(
-      <img
-        className="gif"
-        src={src}
-        alt=""
-      />
+      <img className="gif" src={src} />
     )
   }
 }
